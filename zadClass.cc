aa7d3bdd554aa2cf6a2ee35d712f6d123f82f04e
PatrykMrozowski <162620716+PatrykMrozowski@users.noreply.github.com>
2024-03-21 11:23:17 +0100

    Update and rename klasa to zadClass.cc

---
+++ zadClass.cc
@@ -0,0 +1,15 @@
+#define 
+class przyklad{
+  przyklad();
+  ~przyklad();
+void pierwsza(int rpierwsza)    {fpierwsza = rpierwsza;};
+void druga(int fdruga)    {fpdruga = fpierwsza;};
+void trzecia(int trzecia)    {ftrzecia = trzecia}
+
+int getPierwsza() {return fpierwsza};
+int getDruga()  {return fdruga}
+int getTrzecia()  {return ftrzecia}
+
+void Print()
+
+}
