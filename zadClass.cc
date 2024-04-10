96da7e11972e7386dc92ab5e9edda30a724cedad
PatrykMrozowski <162620716+PatrykMrozowski@users.noreply.github.com>
2024-04-10 13:03:14 +0200

    ZadEnumExample.cc

--- zadClass.cc
+++ zadClass.cc
@@ -1,15 +1,63 @@
-#define 
-class przyklad{
-  przyklad();
-  ~przyklad();
-void pierwsza(int rpierwsza)    {fpierwsza = rpierwsza;};
-void druga(int fdruga)    {fpdruga = fpierwsza;};
-void trzecia(int trzecia)    {ftrzecia = trzecia}
+#include<iostream>
+using namespace std;
 
-int getPierwsza() {return fpierwsza};
-int getDruga()  {return fdruga}
-int getTrzecia()  {return ftrzecia}
+enum class Miesiace {
+    Styczen,
+    Luty,
+    Marzec,
+    Kwiecien,
+    Maj,
+    Czerwiec,
+    Lipiec,
+    Sierpien,
+    Wrzesien,
+    Pazdziernik,
+    Listopad,
+    Grudzien
+};
 
-void Print()
+int main()
+{
+    Miesiace aktualny_miesiac = Miesiace::Kwiecien;
 
+    switch (aktualny_miesiac) {
+        case Miesiace::Styczen:
+            cout << "Styczeń" << endl;
+            break;
+        case Miesiace::Luty:
+            cout << "Luty" << endl;
+            break;
+        case Miesiace::Marzec:
+            cout << "Marzec" << endl;
+            break;
+        case Miesiace::Kwiecien:
+            cout << "Kwiecień" << endl;
+            break;
+        case Miesiace::Maj:
+            cout << "Maj" << endl;
+            break;
+        case Miesiace::Czerwiec:
+            cout << "Czerwiec" << endl;
+            break;
+        case Miesiace::Lipiec:
+            cout << "Lipiec" << endl;
+            break;
+        case Miesiace::Sierpien:
+            cout << "Sierpień" << endl;
+            break;
+        case Miesiace::Wrzesien:
+            cout << "Wrzesień" << endl;
+            break;
+        case Miesiace::Pazdziernik:
+            cout << "Październik" << endl;
+            break;
+        case Miesiace::Listopad:
+            cout << "Listopad" << endl;
+            break;
+        case Miesiace::Grudzien:
+            cout << "Grudzień" << endl;
+            break;
+    }
+
+    return 0;                              
 }
