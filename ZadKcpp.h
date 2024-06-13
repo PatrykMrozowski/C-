878eadeb4a47b56e953dc69a92bcb780b1ac6714
PatrykMrozowski <162620716+PatrykMrozowski@users.noreply.github.com>
2024-06-13 10:36:24 +0200

    Create ZadKcpp.h

---
+++ ZadKcpp.h
@@ -0,0 +1,20 @@
+#ifndef ZADKCPP_H
+#define ZADKCPP_H
+
+class ZadKcpp {
+public:
+
+
+
+void zadanie_1();
+void zadanie_2();
+void zadanie_3();
+void zadanie_4();
+void zadanie_5();
+};
+int main() {
+
+    return 0;
+}
+
+#endif // ZADKCPP_H
